5019bcb164b13d0bc9bae84969e642a690fc1aa8
Swifter in Ubuntu <chengkong2006@126.com>
2016-07-12 16:48:17 +0800

    create a library

---
+++ banner/src/main/java/lic/swifter/banner/LoopingViewPager.java
@@ -0,0 +1,23 @@
+package lic.swifter.banner;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ *
+ * Created by lic on 16-7-12.
+ */
+public class LoopingViewPager extends ViewPager {
+
+    public LoopingViewPager(Context context) {
+        this(context, null);
+    }
+
+    public LoopingViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+
+    }
+}
