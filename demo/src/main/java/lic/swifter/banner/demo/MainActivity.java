10be15278897fd710ed2073dce6fad434f057eb5
Lee-swifter <chengkong2006@126.com>
2019-09-29 11:39:20 +0800

    update dependencies version and build tool version

--- demo/src/main/java/lic/swifter/banner/demo/MainActivity.java
+++ demo/src/main/java/lic/swifter/banner/demo/MainActivity.java
@@ -1,7 +1,7 @@
 package lic.swifter.banner.demo;
 
+import android.app.Activity;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +14,7 @@ import android.widget.Toast;
 import lic.swifter.banner.LoopingPagerAdapter;
 import lic.swifter.banner.LoopingViewPager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private LoopingViewPager loopingViewPager;
     private TextView textView;
